0efa6d418e31ba329c955fff3ade7f0c75775364
CastilloRodrig0 <rcastillo288@gmail.com>
2022-01-17 16:43:53 -0300

    coreccion de archivos

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+const AppRoutes = () => {
+    return (
+        <Router>
+       <Switch>
+           <Route />
+           {/* <Route  component={Error}/> */}
+        </Switch>
+        </Router>
+    )
+}
+export default AppRoutes;
