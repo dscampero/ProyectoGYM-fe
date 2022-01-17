d00d545551bddf99c9e54c57f09a520ccf5385f0
CastilloRodrig0 <rcastillo288@gmail.com>
2022-01-17 19:13:02 -0300

    Creacion de pagina clases

---
+++ src/components/LessonsNavbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const LessonsNavbar = () => {
+    return (
+        <div>
+            <div className="btn-group" role="group" aria-label="Basic example">
+              <button type="button" className="colorbutton1">Entrenamiento Funcional</button>
+              <button type="button" className="btn btn-info">Aero Local</button>
+              <button type="button" className="btn btn-danger">Aero Combat</button>
+              <button type="button" className="btn btn-success">Ritmos</button>
+              <button type="button" className="btn btn-secondary">Yoga</button>
+              <button type="button" className="btn btn-warning">Abdominales</button>
+              <button type="button" className="btn btn-info">Estiramiento</button>
+<p ClassName="colorbutton1">asd</p>
+            </div>
+        </div>
+    )
+}
+
+export default LessonsNavbar
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,12 @@
 
 import React from 'react'
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import LessonsScreen from '../pages/LessonsScreen'
 const AppRoutes = () => {
     return (
         <Router>
        <Switch>
-           <Route />
-           {/* <Route  component={Error}/> */}
+           <Route component={LessonsScreen}/>
         </Switch>
         </Router>
     )
