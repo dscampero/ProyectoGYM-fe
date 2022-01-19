8dd61de31a68a9354f7bed47bea8161b2199d508
CastilloRodrig0 <rcastillo288@gmail.com>
2022-01-19 18:23:53 -0300

    Agregado de pagina HomeScreen

---
+++ src/pages/HomeScreen.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HomeScreen = () => {
+  return <div>Este es el home</div>;
+};
+
+export default HomeScreen;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,10 +2,12 @@
 import React from 'react'
 import {BrowserRouter as Router,Route} from 'react-router-dom'
 import LessonsScreen from '../pages/LessonsScreen'
+import HomeScreen from '../pages/HomeScreen'
 const AppRoutes = () => {
     return (
         <Router>
-        <Route component={LessonsScreen}/>
+            <Route exact path="/" component={HomeScreen}/>
+        <Route exact path="/clases"component={LessonsScreen}/>
         </Router>
     )
 }
