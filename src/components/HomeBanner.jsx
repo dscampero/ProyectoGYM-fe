27858031f84f7dc8931dfbbe8027182ecc6a3eac
dscampero <dscampero@gmail.com>
2022-01-21 20:48:39 -0300

    Agregado de breakpoints para el primer banner del Home

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import Banner from "../assets/HomeBanner1.png";
+import BannerMobile from '../assets/HomeBannerMobile.png'
 
 const HomeBanner = () => {
   return (
     <div>
-      <div class="" id="container-importante" name="container-importante">
-        <div>
+      <div>
+        <div className='bannerfull'>
           <img
             src={Banner}
             alt="bannerHome"
@@ -14,12 +15,20 @@ const HomeBanner = () => {
             style={{ position: "absolute", zIndex: 20 }}
           />
         </div>
+        <div className='bannerMobile'>
+          <img
+            src={BannerMobile}
+            alt="bannerHome"
+            class="img-fluid"
+            style={{ position: "absolute", zIndex: 20}}
+          />
+        </div>
         <div className="bannerContainer">
             <p className="bannerHeader">¡EMPEZÁ HOY!</p>
             <p className="bannerPrice">$3920*</p>
             <p className="bannerPriceMonth">MENSUAL</p>
             <p className="bannerBenefits">ACCEDÉ A TODOS NUESTROS BENEFICIOS</p>
-            <div className="coso">
+            <div className="bannerList">
                 <ul>
                 <li>• Sector cardio</li>
                 <li>• Sector musculación</li>
@@ -29,9 +38,9 @@ const HomeBanner = () => {
                 <li>• Sector Sector sauna</li>
             </ul>
                 </div>
-        </div>
         <div>
           <Button className="bannerbutton">ASOCIATE YA</Button>
+        </div>
         </div>
       </div>
     </div>
