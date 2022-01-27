0da477311ceb11b2f21f28a6b00c8f873aa0d566
CastilloRodrig0 <rcastillo288@gmail.com>
2022-01-27 09:39:50 -0300

    creacion de loginComponent

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,23 +1,96 @@
 import React from 'react';
-import { Form,Button } from 'react-bootstrap';
-
+import logo from '../assets/pngLogin/biceps2.svg'
 const LoginComponent = () => {
-  return <>SOY LOGIN
-<Form >
-  <Form.Group className="mb-3"  controlId="formBasicEmail">
-    <Form.Control type="email" placeholder="Nombre de Usuario" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Control type="password" placeholder="Constraseña" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-  </Form.Group>
-  <Button className="buttonlogin">
-    Ingresar
-  </Button>
-</Form>
-  
-  </>;
-};
+  return (
+  <div  className="formBody ">
+    {/* <div className="container h-100 "> */}
+      <div className="d-flex justify-content-center h-100 ">
+        <div className="userLoginCard ">
+          <div className="d-flex justify-content-center">
+            <div className="loginLogoContainer">
+               <img src={logo} className="loginLogo" alt="Logo" /> <img/>
+            </div>
+          </div>
+          <div className="d-flex justify-content-center formContainer">
+            <form>
+              <h3 className="mt-0" id="registerFormTitle" name="registerFormTitle">
+                COMPLETA PARA INICIAR
+              </h3>
+              <div className="input-group mb-3">
+                <div className="input-group-append">
+                  <span className="input-group-text"
+                    ><i className="fas fa-user"></i
+                  ></span>
+                </div>
+                <input
+                  type="text"
+                  id="userName"
+                  name="userName"
+                  className="form-control input_user"
+                  placeholder="Nombre de Usuario"
+                  required
+                  minLength="8"
+                  maxLength="12"
+                  pattern="[A-Za-z0-9]+"
+                />
+              </div>
+              <div className="input-group mb-2">
+                <div className="input-group-append">
+                  <span className="input-group-text"
+                    ><i className="fas fa-key"></i
+                  ></span>
+                </div>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="form-control input_pass"
+                  placeholder="Contraseña"
+                  required
+				  minLength="8"
+                  maxLength="12"
+                  pattern="[A-Za-z0-9]+"
+                />
+              </div>
+              <div className="form-group">
+
+              </div>
+              <div className="d-flex justify-content-center mt-3 loginContainer">
+                <button
+                  type="submit"
+                  id="loginButton"
+                  name="loginButton"
+                  // onClick="login()"
+                >
+                  <a>INGRESAR</a>
+                </button>
+              </div>
+            </form>
+          </div>
+
+          <div className="mt-0">
+            <div className="d-flex justify-content-center links">
+              ¿No tienes cuenta?
+              <a href="#" className="ml-2">Regístrate</a>
+            </div>
+            <div className="d-flex justify-content-center links">
+              <a href="#">¿Olvidaste tu contraseña?</a>
+            </div>
+            <div className="d-flex justify-content-center links">
+              <a
+                href="#"
+                className="ml-2"
+                id="backToHome"
+                name="backToHome"
+                >VOLVER A INICIO</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    {/* </div> */}
+  </div>
+
+)};
 
 export default LoginComponent;
